b5ac7e4b6c3b38bf56df4ca371f65e01658f1713
JCM401 <juancm8757@gmail.com>
2026-05-12 10:01:12 -0600

    Update db.php

--- db.php
+++ db.php
@@ -15,6 +15,7 @@ $conn = new mysqli($host, $user, $pass, $db, (int)$port);
 if (!$usuario || !$password)
 die(json_encode(["ok"=>false,"msg"=>"Faltan datos"]));
 if ($conn->connect_error) {
-
+http_response_code(500);
+die(json_encode(["ok" => false, "msg" => $conn->connect_error]));
 } $conn-
 >set_charset("utf8mb4");
